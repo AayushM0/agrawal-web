9ad03be006f5a641f65dc19a2117c91a7b96c24e
Aayush Mittal <aayushmittal620@gmail.com>
2026-09-04 16:20:41 +0530

    chore: remove scratch scripts, tasks, and internal docs from git tracking

--- fix-ts.mjs
+++
@@ -1,9 +0,0 @@
-
-import fs from "fs";
-let code = fs.readFileSync("src/actions/otp.ts", "utf8");
-code = code.replace(
-  "export interface SendOtpInput {\n  recipient: string;\n  type?: \"sms\" | \"email\";\n}",
-  "export interface SendOtpInput {\n  recipient: string;\n  type?: string;\n}"
-);
-fs.writeFileSync("src/actions/otp.ts", code);
-
